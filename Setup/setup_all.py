5f861a662fec28db08bd32047273755d6d0c7a5e
sivaram <satyaman@usc.edu>
2019-04-23 13:11:25 -0700

    sivaram

--- Setup/setup_all.py
+++ Setup/setup_all.py
@@ -22,38 +22,35 @@ import os
 
 def init_database(nodes,is_client):
 	cmd="sudo python ./init.py usc558l"
-	result = subprocess.run(cmd.split(), stdout=subprocess.PIPE)
-	print result
+	os.system(cmd)
 	if is_client==1:
 		return
 	for node,node_data in nodes.iteritems():
 		cmd="sudo python ./insert_topo.py "+node+" "+node_data["links_to"]+" "+str(node_data["self"])
-		result = subprocess.run(cmd.split(), stdout=subprocess.PIPE)
+		os.system(cmd)
 
 def add_client_entries(as_name,server_url,links_to,self):
 	if links_to=="None":
 		return
 	cmd="sudo python ./insert_senss_client.py usc558l "+as_name+" "+server_url+" "+links_to+" "+self
-	result = subprocess.run(cmd.split(), stdout=subprocess.PIPE)
+	os.system(cmd)
 
 def copy_files():
 	cmd="sudo cp -rf Server/ /var/www/html/"
-	result = subprocess.run(cmd.split(), stdout=subprocess.PIPE)
+	os.system(cmd)
 
 	cmd="sudo cp -rf Client/ /var/www/html/"
-	result = subprocess.run(cmd.split(), stdout=subprocess.PIPE)
-
+	os.system(cmd)
 
 def install_dependencies():
 	cmd="sh ./install_dependencies.sh"
-	result = subprocess.run(cmd.split(), stdout=subprocess.PIPE)
-
+	os.system(cmd)
 	print "Installed dependencies"
 
 #Need to modify this to be supportive for multiple elements
 def start_monitor_flows(multiply,legit_address):
 	cmd="screen -d -m sudo python /var/www/html/SENSS/UI_client_server/Server/monitor_flows.py "+str(multiply)+" "+str(legit_address)
-	result = subprocess.run(cmd.split(), stdout=subprocess.PIPE)
+	os.system(cmd)
 	print "Started Monitoring flows controller"
 
 def print_data(data):
@@ -64,11 +61,11 @@ def copy_certificates(server_flag,node):
 	print "Copying certificates"
 	if server_flag==True:
         	cmd="sudo cp /proj/SENSS/SENSS_git/SENSS/UI_client_server/GenCertificates/certificates/rootcert.pem /var/www/html/SENSS/UI_client_server/Server/cert/rootcert.pem"
-		result = subprocess.run(cmd.split(), stdout=subprocess.PIPE)
+			os.system(cmd)
 	else:
 		certificate_to_copy=node+"cert.pem"
-	        cmd="sudo cp /proj/SENSS/SENSS_git/SENSS/UI_client_server/GenCertificates/certificates/"+certificate_to_copy+" /var/www/html/SENSS/UI_client_server/Client/cert/clientcert.pem"
-		result = subprocess.run(cmd.split(), stdout=subprocess.PIPE)
+	    cmd="sudo cp /proj/SENSS/SENSS_git/SENSS/UI_client_server/GenCertificates/certificates/"+certificate_to_copy+" /var/www/html/SENSS/UI_client_server/Client/cert/clientcert.pem"
+		os.system(cmd)
 
 def configure_nodes():
 	nodes={}
@@ -113,8 +110,7 @@ def configure_nodes():
 		copy_files()
 
 		cmd="sudo service apache2 restart"
-		result = subprocess.run(cmd.split(), stdout=subprocess.PIPE)
-
+		os.system(cmd)
 
 if __name__ == '__main__':
 	configure_nodes()
